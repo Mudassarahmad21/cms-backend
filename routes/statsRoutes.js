3868394d4a90409968574be7977a93288e0892b2
Mudassar Ahmad <mlkmud56@gmail.com>
2026-02-10 23:05:56 +0500

    Cleaning and polishing the code

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getDashboardStats } = require("../controllers/statsController");
 const { protect, authorize } = require("../middleware/authMiddleware");
 
-// All roles can see stats, but usually restricted to Admin/Farmer/Broker
+
 router.get("/", protect, authorize("ADMIN", "FARMER", "BROKER"), getDashboardStats);
 
 module.exports = router;
